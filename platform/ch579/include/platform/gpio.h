0da8f4c85cafb926a212e850d01e93e662475b5c
Sheep Sun <sunxiaoyang2003@gmail.com>
2021-11-15 22:25:47 +0800

    Fix GPIO mapping

--- platform/ch579/include/platform/gpio.h
+++ platform/ch579/include/platform/gpio.h
@@ -4,7 +4,7 @@
 #define GPIO_OUTPUT_PP_20 ((1 << 16) << 2)
 
 //PA[0]-PA[15] -> GPIO 1-16, PB[0]-PB[23] -> GPIO 17 - 40
-#define GPIO(port, pin) (16 * port + pin)
+#define GPIO(port, pin) (16 * port + pin + 1)
 
 #define GPIO_PORT_A 0
 #define GPIO_PORT_B 1
