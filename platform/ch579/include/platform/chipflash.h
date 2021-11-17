39c3ab00159415bdd3e248d1e4e6f980cd758472
Sheep Sun <sunxiaoyang2003@gmail.com>
2021-11-17 11:57:47 +0800

    CH579:Add sysparam using dataflash

--- platform/ch579/include/platform/chipflash.h
+++ platform/ch579/include/platform/chipflash.h
@@ -2,9 +2,12 @@
 
 #define CODE_FLASH_BASE 0
 #define CODE_FLASH_SIZE 0x3E800
+#define CODE_FLASH_NAME "codeflash"
 #define DATA_FLASH_BASE 0x3E800
 #define DATA_FLASH_SIZE 0x800
+#define DATA_FLASH_NAME "dataflash"
 #define INFO_FLASH_BASE 0x40000
 #define INFO_FLASH_SIZE 0x400
+#define INFO_FLASH_NAME "infoflash"
 
 void ch579_chipflash_init(void);
--- target/ch579/target.c
+++ target/ch579/target.c
@@ -1,6 +1,12 @@
 #include <target.h>
 #include <target/debugconfig.h>
+#include <lib/sysparam.h>
 #include <platform/gpio.h>
+#include <platform/chipflash.h>
+
+#define PARAM_BDEV_NAME DATA_FLASH_NAME
+#define PARAM_OFFSET 0
+#define PARAM_SIZE DATA_FLASH_SIZE
 
 void target_early_init(void) {
 }
@@ -9,5 +15,7 @@ void target_set_debug_led(unsigned int led, bool on) {
 }
 
 void target_init(void) {
+    bdev_t *flash = bio_open(PARAM_BDEV_NAME);
+    sysparam_scan(flash, PARAM_OFFSET, PARAM_SIZE);
 }
 
